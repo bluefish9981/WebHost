1969845d3e1fae85190c7ed864707bbd0195d82f
Bluefish <bluefish9981@gmail.com>
2020-04-09 15:04:53 -0500

    Modifications to HTML input methods.

--- app/src/main/java/com/example/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -1,14 +1,47 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
+        import android.text.Editable;
+        import android.text.TextWatcher;
+        import android.widget.EditText;
+        import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    TextView rows;
+    EditText input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        rows = (TextView) findViewById(R.id.rows);
+        input = (EditText) findViewById(R.id.input);
+
+        input.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                int lines = input.getLineCount();
+                String lineText = "";
+                for(int z = 1; z <= lines; z++)
+                {
+                    lineText = lineText + z + "\n";
+                }
+                rows.setText(lineText);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
