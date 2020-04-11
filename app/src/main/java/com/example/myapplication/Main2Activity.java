a2ea6e29afa597538621fd2571a997b30ae1d19f
Bluefish <bluefish9981@gmail.com>
2020-04-10 19:49:09 -0500

    Fixed critical error that disabled the second view.

--- app/src/main/java/com/example/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -1,17 +1,49 @@
 package com.example.myapplication;
 
-        import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-        import android.os.Bundle;
-        import android.text.Editable;
-        import android.text.TextWatcher;
-        import android.widget.EditText;
-        import android.widget.TextView;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class Main2Activity extends AppCompatActivity {
 
+    /*if (ContextCompat.checkSelfPermission(thisActivity, Manifest.permission.WRITE_CALENDAR)
+            != PackageManager.PERMISSION_GRANTED) {
+        // Permission is not granted
+    }*/
+
+    private int STORAGE_RPERMISSION_CODE = 1;
+    private int STORAGE_WPERMISSION_CODE = 2;
+
+    private static final String FILE_NAME = "example.html";
+
     TextView rows;
     EditText input;
+    Button save;
+    String htmlSavePath = "/storage/emulated/0/HTML";
+    OutputStream outputStream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +52,7 @@ public class Main2Activity extends AppCompatActivity {
 
         rows = (TextView) findViewById(R.id.rows);
         input = (EditText) findViewById(R.id.input);
+        save = (Button) findViewById(R.id.save);
 
         input.addTextChangedListener(new TextWatcher() {
             @Override
@@ -43,5 +76,56 @@ public class Main2Activity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private boolean isExternalStorageWriteable()
+    {
+        if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState()))
+        {
+            Log.i("State", "Yes, it is writeable!");
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            Log.i("State", "Yes, it is readable!");
+            return true;
+        } else {
+        return false;}
+    }
+
+    public static boolean isExternalStorageMounted() {
+
+        String dirState = Environment.getExternalStorageState();
+        if(Environment.MEDIA_MOUNTED.equals(dirState))
+        {
+            return true;
+        }else
+        {
+            return false;
+        }
+    }
+
+    //Context context = getApplicationContext();
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void save(View v) {
+        String htmlcode = input.getText().toString();
+        FileOutputStream fos = null;
+        if(isExternalStorageWriteable()){
+            File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+            File file = new File(dir, "example.html");
+
+            try (FileWriter fileWriter = new FileWriter(file)) {
+                fileWriter.write(htmlcode);
+            } catch (IOException e) {}
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_WPERMISSION_CODE);
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,32 +1,68 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private int STORAGE_RPERMISSION_CODE = 1;
+    private int STORAGE_WPERMISSION_CODE = 2;
     private Button newView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Context context = MainActivity.this;
+
+        Button requestPermissions = findViewById(R.id.permissions);
+        requestPermissions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(context,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+                {
+                    Toast.makeText(context, "Permission for external storage granted.", Toast.LENGTH_LONG).show();
+                }
+                else if (ContextCompat.checkSelfPermission(context,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+                {
+                    Toast.makeText(context, "Permission for external storage denied.", Toast.LENGTH_LONG).show();
+                    requestWPermission();
+                }
+            }
+        });
 
         newView = (Button) findViewById(R.id.newView);
         newView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                open2ndActivity();
+                openActivity2();
             }
         });
     }
 
-    public void open2ndActivity() {
+    public void openActivity2()
+    {
         Intent intent = new Intent(this, Main2Activity.class);
         startActivity(intent);
+    }
+
+    private void requestWPermission()
+    {
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_WPERMISSION_CODE);
     }
 }
